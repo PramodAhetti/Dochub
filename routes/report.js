7c79106733a946b419f9200369a59bd386172a87
MacAhetti <macahetti@gmail.com>
2024-03-31 00:09:59 +0530

    completed the new,upload and delete route

--- routes/report.js
+++ routes/report.js
@@ -1,111 +1,118 @@
-const express=require('express')
-const router=express.Router();
-const userauth=require('../middleware/auth').userauth;
-const doctorauth=require('../middleware/auth').doctorauth;
-const reports=require('../models/Report');
-const pdf=require('../models/files');
-const multer=require('multer');
+const express = require('express')
+const router = express.Router();
+const userauth = require('../middleware/auth').userauth;
+const doctorauth = require('../middleware/auth').doctorauth;
+const reports = require('../models/Report');
+const pdf = require('../models/files');
+const multer = require('multer');
 
 
-const upload=multer();
+const upload = multer();
 
 //get api/users
 //test route
 //route to get all the posts
 router.post('/myreport', userauth, async (req, res) => {
-   try{
-    const allposts=await reports.find({ownedId:req.body.userId}) 
-    res.json({data:allposts});
-}catch(err){
-    res.status(400).send({error:err})
-   } 
+    try {
+        const allposts = await reports.find({ ownedId: req.body.userId })
+        res.json({ data: allposts });
+    } catch (err) {
+        res.status(400).send({ error: err })
+    }
 });
 
 
-//route to add new post
-// router.post('/new',userauth,async (req,res)=>{
-//      try{
-//         let newfile=new file({
-//             data:req.body.report
-//         })
-//         await newfile.save();
-//         let newReport=new reports({
-//             ownedId:req.body.userId,
-//             report:newfile.id,
-//             date:Date.now()
-//          });
-//          await newReport.save();
-//          return res.send({msg:'post saved'});
-//      }catch(err){
-//         res.status(400).send({error:"server error"});
-//         console.log(err);    
-//     }
+router.post('/new', upload.single('file'), userauth, async (req, res) => {
+    try {
+        if (!req.file) {
+            throw "No file was uploaded";
+        }
+        const report = req.file.buffer.toString('base64');
+        console.log(report);
+        let newfile = new pdf({
+            data:report 
+        })
+        await newfile.save();
+        let newReport = new reports({
+            ownedId: req.body.userId,
+            report: newfile.id,
+            message: req.body.message,
+            date: Date.now()
+        });
+        await newReport.save();
+        return res.send({ msg: 'post saved' });
+    } catch (err) {
+        res.status(400).send({ error: err});
+        console.log(err);
+    }
+});
 
-// });
-
-//do from here tomorrow
-
-router.post('/upload',upload.single('file'),doctorauth,async(req,res)=>{
-    try{
-     if(!req.file){
-        throw "No file was uploaded";
-     }
-     const report=req.file.buffer.toString('base64');
-     console.log(report);
-     const ownedId=req.body.clientId;
-     let newpdf=new pdf({
-        data:report
-     }) 
-     await newpdf.save();
-     let newReport=new reports({
+router.post('/upload', upload.single('file'), doctorauth, async (req, res) => {
+    try {
+        if (!req.file) {
+            throw "No file was uploaded";
+        }
+        const report = req.file.buffer.toString('base64');
+        console.log(report);
+        const ownedId = req.body.clientId;
+        let newpdf = new pdf({
+            data: report
+        })
+        await newpdf.save();
+        let newReport = new reports({
             ownedId,
-            report:newpdf.id,
-            message:req.body.message,
-            date:Date.now()
-         });
-         await newReport.save();  
+            report: newpdf.id,
+            message: req.body.message,
+            date: Date.now()
+        });
+        await newReport.save();
 
-     res.send({"msg":"saved the report"})
+        res.send({ "msg": "saved the report" })
 
-    }catch(err){
-        res.status(400).send({error:err})
+    } catch (err) {
+        res.status(400).send({ error: err })
     }
 })
 //route to delete already existing posts
-router.post('/delete',userauth,async (req,res)=>{
-    try{
-        let post=await reports.findById(req.body.postId)
-        if(post){
-            post.delete();
-            return res.send({msg:"deleted the post"})
-        }else{
-            throw "Post not found"; 
+router.post('/delete', userauth, async (req, res) => {
+    try {
+        let post = await reports.findById(req.body.postId)
+        if (post) {
+            console.log(post);
+            let files = await pdf.findById(post.report);
+            console.log(files)
+            files.delete()
+            post.delete()
+            return res.send({ msg: "deleted the post" })
+        } else {
+            throw "Post not found";
         }
-    }catch(err){
-       res.status(400).send({error:err});
+    } catch (err) {
+        console.log(err)
+        res.status(400).send({ error: err });
     }
 
 });
 
-router.post('/addcomment',userauth,async(req,res)=>{
-    try{
-       let report=await reports.findById(req.body.postId);
-       report.comment.push({user:req.body.userId,comment:req.body.comment});
-       await report.save();
-       res.send({msg:"commented to this post"})
-    }catch(err){
+router.post('/addcomment', userauth, async (req, res) => {
+    try {
+        let report = await reports.findById(req.body.postId);
+        report.comment.push({ user: req.body.userId, comment: req.body.comment });
+        await report.save();
+        res.send({ msg: "commented to this post" })
+    } catch (err) {
         console.log(err);
-      res.status(400).send({error:"server error"})
+        res.status(400).send({ error: "server error" })
     }
 })
 
-router.post('/test',async(req,res)=>{
-    try{
-      let data=await reports.findOne({ownedId:req.body.id});
-      
-      res.send(data);
-    }catch(err){
-        res.status(400).send({error:err})
+router.post('/test', async (req, res) => {
+    try {
+        let data = await reports.findOne({ ownedId: req.body.id });
+
+        res.send(data);
+    } catch (err) {
+        res.status(400).send({ error: err })
     }
 })
 
@@ -126,4 +133,4 @@ router.post('/test',async(req,res)=>{
 //     res.status(400).send({error:"server error"})
 //   }
 // })
-module.exports=router;
+module.exports = router;
